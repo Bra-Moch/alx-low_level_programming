6b6c8702d14f8f52f9ce70a40afd487d48438461
Bra Moch <quarm33@gmail.com>
2023-03-17 19:07:21 -0700

    8. Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: prints all the numbers of base 16 in lowercase and a new line.
+ * Return: 0
+ */
+
+int main(void)
+{
+	char digit;
+	char letter;
+
+	for (digit = '0'; digit < '10'; digit++)
+		putchar(digit + '0');
+
+	for (letter = 'a'; letter <= 'f'; letter++)
+		putchar(letter);
+
+	putchar('\n');
+
+	return (0);
+
+}
