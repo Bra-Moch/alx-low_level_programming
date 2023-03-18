cd49cef5459284f9097ad50271e595b17e8e8aef
Bra Moch <quarm33@gmail.com>
2023-03-17 19:48:44 -0700

    9. Patience

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,18 +7,20 @@
 
 int main(void)
 {
-    int num;
-    for (num = 0; num < 10; num++)
-    {
-        putchar(num + '0');
+	int num;
 
-        if (num != 9)
-        {
-            putchar(',');
-            putchar(' ');
-        }
-    }
+	for (num = 0; num < 10; num++)
+	{
+		putchar(num + '0');
+		if (num != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
 
-    putchar('\n');
-    return (0);
+	putchar('\n');
+
+	return (0);
+
 }
