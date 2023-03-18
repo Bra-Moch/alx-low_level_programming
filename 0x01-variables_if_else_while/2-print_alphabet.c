953e7ab033f74c9972622a788541c69d3fc154fe
Bra Moch <quarm33@gmail.com>
2023-03-17 17:19:32 -0700

    alphabet in lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Writing a program that prints the alphabet in lowercase
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char c';
+	for (c = 'a'; <= 'z'; c++)
+		putchar(c);
+
+	putchar('\n');
+
+	return (0);
+}
