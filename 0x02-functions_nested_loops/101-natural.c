0390b7622d8a7d42dae82a23d8366ce786201c93
Bra Moch <quarm33@gmail.com>
2023-03-22 03:04:05 -0700

    The sum of these multiples is 23

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - program that prints the sum of all the multiples of 3 or 5 below 1024
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+	int sum;
+
+	sum = 0;
+
+
+	for (i = 0; i < 10; i++)
+	{
+		if ((i % 3) || (i % 5 == 0))
+		{
+			sum = sum + i;
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
+}
