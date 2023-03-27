2b24cee0e4b55916c1970f06220d0d7d8b2cb7b9
Bra Moch <quarm33@gmail.com>
2023-03-27 14:07:34 -0700

    prints n elements of an array of integers

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -11,12 +11,14 @@
 
 void print_array(int *a, int n)
 {
-	int k;
+	int i;
 
-	for (k = 0; k < n; k+)
-		if k != n - 1)
-		printf("%d, ", a[k]);
+	for (i = 0; i < n; i++)
+	{
+		if (i != (n - 1))
+			printf("%d, ", a[i]);
 		else
-			printf("%d", a[k]);
+			printf("%d", a[i]);
+	}
 printf("\n");
 }
