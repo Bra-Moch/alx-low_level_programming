2126f2a82db68a8aad5fe583df1812308f75dbf7
Bra Moch <quarm33@gmail.com>
2023-03-28 09:39:32 -0700

    generates random valid passwords for the program

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define PASSWORD_LENGTH 10
+/**
+ * main - Generates random valid passwords for the program 101-crackme.
+ *
+ * Return: 0
+ *
+ */
+
+int main(void)
+{
+	int i, total = 2772;
+	char password[pass_len + 1];
+	int ascii_value;
+
+	srand(time(NULL));
+	/* initialize random number generator */
+
+	for (i = 0; i < pass_len; i++)
+	{
+		ascii_value = rand() % 125 + 1;
+		/* generate a random ASCII value between 1 and 125 (inclusive)*/
+
+		printf("%c", ascii_value);
+		total -= ascii_value;
+	}
+
+	printf("%c", total);
+	return (0);
+}
