f40f24db76a227424ed4bea81fb28fe02d0683e6
Bra Moch <quarm33@gmail.com>
2023-04-17 14:49:08 -0700

    0. Poppy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,14 +1,17 @@
 #ifndef DOG_H
 #define DOG_H
 
+/*
+ * File: dog.h
+ * Desc: Header file that defines a new type struct dog.
+ */
+
 /**
- * struct dog - dog structure
- *
- * @name: name of dog
- * @age: age of the dog
- * @owner: dog owner
- *
-*/
+ * struct dog - A new type describing a dog.
+ * @name: The name of the dog.
+ * @age: The age of the dog.
+ * @owner: The owner of the dog.
+ */
 
 struct dog
 {
@@ -18,8 +21,8 @@ struct dog
 };
 
 /**
- * dog_t -Typedef for struct dog
-*/
+ * dog_t - Typedef for struct dog
+ */
 
 typedef struct dog dog_t;
 
